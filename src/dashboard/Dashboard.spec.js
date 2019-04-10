4acc764ceb4439afdb182939e5c6c52d5d576267
cori-peck <cori.peck@ymail.com>
2019-04-10 15:22:10 -0500

    project start, snapshot created

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+import Dashboard from './Dashboard.js';
+
+
+describe('<Dashboard />', () => {
+
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    })
+})
